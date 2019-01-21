5593c7322b50431a6db25522d4037ec2f922f884
dharcourt <dharcourt@gaig.com>
2019-01-20 21:09:35 -0500

    practice

---
+++ lists_tuples_sets.py
@@ -0,0 +1,11 @@
+my_variable = "hello"
+
+grades = [77,80,90,95,100]
+
+tuple_grades = (77,80,90,95,100,105,107,108,109) #immutable
+
+set_grades = {77,80,90,100,100} # unique & unordered
+
+print(sum(grades)/len(grades))
+
+print(set_grades)
