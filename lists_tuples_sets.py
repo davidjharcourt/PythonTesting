f377a890495920c48d2e4eeadcc87eb76e64061b
Harcourt <dharcourt@GAIG.COM>
2019-01-25 08:39:08 -0500

    class additions

--- lists_tuples_sets.py
+++ lists_tuples_sets.py
@@ -8,10 +8,15 @@ set_grades = {77,80,90,100,100} # unique & unordered
 
 grades.append(100)
 
-print(sum(grades)/len(grades))
+#print(sum(grades)/len(grades))
 
+#print(set_grades)
+
+tuple_grades = tuple_grades + (100,) #must have the comma here
+#print(tuple_grades)
+
+grades[0] = 60 #assign index
+print(grades[0]) #index number
+
+set_grades.add(60)
 print(set_grades)
-
-tuple_grades = tuple_grades + (100,)
-
-print(tuple_grades)
