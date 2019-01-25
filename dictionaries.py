5d011da49bde00a483589fea4cf9d97ca8d86f1d
Harcourt <dharcourt@GAIG.COM>
2019-01-25 15:31:26 -0500

    new classes

---
+++ dictionaries.py
@@ -0,0 +1,23 @@
+my_set = {1, 3, 5}
+my_dict = {'name': 'Jose', 'age': 90}
+another_dict = {1: 15, 2: 75, 3: 150}
+
+# Dictionary = Key Value set, usually used to represent 'things'
+
+lottery_player = {
+    'name': 'Rolf',
+    'numbers': (12, 45, 66, 23, 22)
+}
+
+universities = [
+    {
+        'name': 'Oxford',
+        'location': 'UK'
+    },
+    {
+        'name': 'MIT',
+        'location': 'US'
+    }
+]
+
+print(sum(lottery_player['numbers']))
--- if_statements.py
+++ if_statements.py
@@ -24,22 +24,23 @@ def who_do_you_know():
     # Ask the user for a list of people they know
     people = input("Enter a list of names you know, separated by commas:  ")
     # Split the string into a list
-    people_list = people.split(",")
+    #people_list = people.split(",")
     # Trim spaces from people
-    people_without_spaces = []
-    for person in people_list:
-        people_without_spaces.append(person.strip())
+    # people_without_spaces = []
+    # for person in people_list:
+    #     people_without_spaces.append(person.strip())
+    people_without_spaces = [person.strip() for person in people.split(",")]
     # Return that list
     return people_without_spaces
 
 def ask_user():
     # Ask user for a name
-    person = input("What name would you like to check? ")
+    input_person = input("What name would you like to check? ")
     #See if their name is in the list of people they know
     # Print out that they know the person
-    if person in who_do_you_know():
-        print("You know {}!".format(person))
+    if input_person in who_do_you_know():
+        print("You know {}!".format(input_person))
     else:
-        print("You don't know {}".format(person))
+        print("You don't know {}".format(input_person))
 
 ask_user()
