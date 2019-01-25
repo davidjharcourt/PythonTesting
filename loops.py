901a83f1c5d8e94669ba6730d0da09e6c4e96988
Harcourt <dharcourt@GAIG.COM>
2019-01-25 09:47:49 -0500

    class changes

--- loops.py
+++ loops.py
@@ -1,3 +1,17 @@
-my_variable = "hello"
+# my_variable = "hello"
+#
+# for character in my_variable: #iterables are strings,lists, sets, tuples
+#     print(character)
+#
+# my_list = [1,3,5,7,9]
+#
+# for number in my_list:
+#     print(number ** 2)
 
-print(my_variable[0])
+user_wants_number = True
+while user_wants_number == True:
+    print(10)
+
+    user_input = input("Should we print again? (y/n) ")
+    if user_input == 'n':
+        user_wants_number = False
