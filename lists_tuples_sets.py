1d30ea0eda685a323d279f5f2171d71860ccdc97
Harcourt <dharcourt@GAIG.COM>
2019-01-25 08:33:31 -0500

    changes to lists tuples sets

--- lists_tuples_sets.py
+++ lists_tuples_sets.py
@@ -6,6 +6,12 @@ tuple_grades = (77,80,90,95,100,105,107,108,109) #immutable
 
 set_grades = {77,80,90,100,100} # unique & unordered
 
+grades.append(100)
+
 print(sum(grades)/len(grades))
 
 print(set_grades)
+
+tuple_grades = tuple_grades + (100,)
+
+print(tuple_grades)
