c75a1283493e6fa2d996096a061396d8d2e6f718
Harcourt <dharcourt@GAIG.COM>
2019-01-25 13:16:00 -0500

    class changes

---
+++ list_comprehension.py
@@ -0,0 +1,11 @@
+my_list = [0, 1, 2, 3, 4]
+
+my_equal_list = [x for x in range(5)]
+
+multiply_list = [x*3 for x in range(5)]
+
+#print(multiply_list)
+
+print(8 % 3) #modulus operator
+
+print([n for n in range(10) if n % 2 == 0])
