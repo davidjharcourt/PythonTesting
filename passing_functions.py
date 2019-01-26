a38e10f4fb02bd8adf006765fce5e281d165248f
Harcourt <dharcourt@GAIG.COM>
2019-01-26 07:49:59 -0500

    more classes complete

---
+++ passing_functions.py
@@ -0,0 +1,22 @@
+def methodception(another):
+    return another()
+
+def add_two_numbers():
+    return 35 + 77
+
+# print (methodception(add_two_numbers))
+
+# lambda anonymous function in one line
+# print(methodception(lambda: 35 + 77))
+# declarative/functional programming, good for working with data
+# can I mentally combine this with SQL?
+
+my_list = [13, 56, 77, 484]
+print(list(filter(lambda x: x != 13, my_list)))
+
+(lambda x: x * 3)(5)
+
+def f(x):
+    return x * 3
+
+f(5)
